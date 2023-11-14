b8efe91b8bb1dac8306d1639dec15b6105229a3f
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2023-11-13 17:36:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
 import time
+isBrowserOpen = False
+
+
 
 def main():
-    try:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("https://www.google.com/")
-        oldURL = driver.current_url
-        print(oldURL)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://www.google.com/")
+    isBrowserOpen = True
+    oldURL = driver.current_url
+    print(oldURL)
+    
+    title = driver.find_element(By.TAG_NAME, "title").get_attribute('textContent')
+    print(f"Title: {title}")
 
+    try:
         while True:
             time.sleep(1)
-            current_url = driver.current_url
-            if current_url != oldURL:
-                oldURL = current_url
+            driver.current_url
+            if driver.current_url != oldURL:
+                oldURL = driver.current_url
                 print(oldURL)
-    except WebDriverException as e:
-        print("error:", e)
-    finally:
-        driver.quit()
-        print("Browser was closed by user.")
+                title = driver.find_element(By.TAG_NAME, "title").get_attribute('textContent')
+                print(f"Title: {title}")
 
-if __name__ == "__main__":
-    main()
+        try:
+            main_article = driver.find_element(By.TAG_NAME, "article").get_attribute('outerHTML')
+            print(f"Main Article: {main_article}")
+        except:
+            print("Main article element not found.")
+        driver.quit()
+
+    except WebDriverException:
+        isBrowserOpen = False
+        print("Browser was closed by the user.")
+
+
+
+main()
