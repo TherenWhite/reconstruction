a0ed8025382a5e5d0f252eb14f2439b02bf6f503
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2023-11-13 17:10:31 -0700

    main.py fixes

--- main.py
+++ main.py
@@ -2,40 +2,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import WebDriverException
 import time
-isBrowserOpen = False
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# async def browserTracking():
-#     try:
-#         while True:
-#             time.sleep(1)
-#             driver.current_url
-#             if driver.current_url != oldURL:
-#                 oldURL = driver.current_url
-#                 print(oldURL)
-#     except WebDriverException:
-#         isBrowserOpen = False
-#         print("Browser was closed by the user.")
-
-
-
-def main(): 
-    driver.get("https://www.google.com/")
-    isBrowserOpen = True
-    oldURL = driver.current_url
-    print(oldURL)
-    
+def main():
     try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get("https://www.google.com/")
+        oldURL = driver.current_url
+        print(oldURL)
+
         while True:
             time.sleep(1)
-            driver.current_url
-            if driver.current_url != oldURL:
-                oldURL = driver.current_url
+            current_url = driver.current_url
+            if current_url != oldURL:
+                oldURL = current_url
                 print(oldURL)
-    except WebDriverException:
-        isBrowserOpen = False
-        print("Browser was closed by the user.")
+    except WebDriverException as e:
+        print("error:", e)
+    finally:
+        driver.quit()
+        print("Browser was closed by user.")
 
-    
-
-main()
+if __name__ == "__main__":
+    main()
