0cfd4c2be223735632337b840408a60918041b9b
Okamura-Yu <150304498+Okamura-Yu@users.noreply.github.com>
2023-11-13 17:32:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
 import time
 isBrowserOpen = False
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# async def browserTracking():
-#     try:
-#         while True:
-#             time.sleep(1)
-#             driver.current_url
-#             if driver.current_url != oldURL:
-#                 oldURL = driver.current_url
-#                 print(oldURL)
-#     except WebDriverException:
-#         isBrowserOpen = False
-#         print("Browser was closed by the user.")
 
 
 
-def main(): 
+def main():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.google.com/")
     isBrowserOpen = True
     oldURL = driver.current_url
     print(oldURL)
-    html_content = driver.page_source
-    print(html_content)
     
+    title = driver.find_element(By.TAG_NAME, "title").get_attribute('textContent')
+    print(f"Title: {title}")
+
     try:
         while True:
             time.sleep(1)
@@ -34,10 +24,18 @@ def main():
             if driver.current_url != oldURL:
                 oldURL = driver.current_url
                 print(oldURL)
+
+        try:
+            main_article = driver.find_element(By.TAG_NAME, "article").get_attribute('outerHTML')
+            print(f"Main Article: {main_article}")
+        except:
+            print("Main article element not found.")
+        driver.quit()
+
     except WebDriverException:
         isBrowserOpen = False
         print("Browser was closed by the user.")
 
-    
+
 
 main()
